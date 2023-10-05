9623d74f84c741316acf8b2b1b507a99f9a65325
jefferson <panjhesse18@gmail.com>
2023-10-05 20:56:58 -0300

    criando views

--- projeto_rec/settings.py
+++ projeto_rec/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "Receita",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,11 @@ ROOT_URLCONF = 'projeto_rec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            BASE_DIR / "base_templates"
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
